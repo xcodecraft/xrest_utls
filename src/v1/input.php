61e9c37d021d25b8d29d8fd0f1676f4c79d72399
zuowenjian <zuowenjian@ayibang.com>
2016-10-02 20:44:10 +0800

    work

--- src/v1/input.php
+++ src/v1/input.php
@@ -5,41 +5,59 @@ class XInput
 {
     static $preg  = "/[\',:;*?~`!#$%^&+=)(<>{}]|\]|\[|\/|\\\|\"|\|/";
     static $inputSettings = array() ;
+    static $exceptionCls  = "RuntimeException" ;
 
-    static public function useSetting($jsonFile)
+    static public function ruleSetting($jsonFile)
     {
         $content = file_get_contents($jsonFile);
         static::$inputSettings = json_decode($content) ;
 
     }
-    public function error($key)
+    static public function failSetting($exceptionCls = "RuntimeException") 
+    {
+        static::$exceptionCLS = $exceptionCls ;
+
+    }
+    static function error($key)
     {
         $msg = "$key 输入错误" ;
-        if(isset(self::$messages[$key]))
+        if(isset(self::$inputSettings[$key]))
         {
+
             $msg = self::$messages[$key];
         }
-        throw new XUserInputException($msg);
+        throw new static::$exceptionCLS($msg);
     }
-    public function safeGet($data,$maps) {
+    static public function safeGet($data,$maps) {
         $mapArr  = explode(',',$maps);
         $ret     = array();
         foreach($mapArr as $key){
-            $value = trim($this->data[$key]) ;
-            if(empty($value) || preg_match(self::$preg,$value)){
-                $this->error($key);
+            $value = trim($data[$key]) ;
+            $setting =  self::$inputSettings['default'] ;
+            if (isset(self::$inputSettings[$key] ))
+            {
+                $setting = self::$inputSettings[$key] ;
+            }
+            if(!preg_match($setting['regex'],$value))
+            {
+                if (null != static::$exceptionCls)
+                {
+                    throw new static::$exceptionCLS($setting['error']);
+
+                }
+                $value  = null ;
             }
             $ret[$key] = $value;
         }
         return $ret ;
     }
 
-    public function safeDict($maps) {
-        $retval =  $this->safeGet($maps);
+    static public function safeDict($maps) {
+        $retval =  static::safeGet($maps);
         return $retval ;
     }
-    public function safeArr($maps) {
-        $retval =  array_values($this->safeGet($maps));
+    static public function safeArr($maps) {
+        $retval =  array_values(static::safeGet($maps));
         return $retval ;
     }
 }
--- test/tc/demo_tc.php
+++
@@ -1,8 +0,0 @@
-<?php
-class DemoTC extends PHPUnit_Framework_TestCase
-{
-    public function testDemo() {
-        $this->assertTrue(true) ;
-    }
-}
-    
